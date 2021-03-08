54e597b5f9076778f463cbfdc1d9c0f0241f9b8f
Satyajit Pattnaik <pattnaiksatyajit89@gmail.com>
2021-03-08 18:29:55 +0800

    Update flask-alexa.py

--- flask-alexa.py
+++ flask-alexa.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 23:18:11 2021
+
+@author: pattn
+"""
+
 from flask import Flask,render_template,redirect,request
 import warnings
 warnings.filterwarnings('ignore')
@@ -10,34 +17,22 @@ import datetime
 import pyjokes
 import wikipedia
 import sys
+import requests, json 
 
 listener = sr.Recognizer()
-engine = pyttsx3.init()
+#engine = pyttsx3.init()
 
-
-voices = engine.getProperty("voices")
-engine.setProperty('voice', voices[1].id)
-
-
-
-
-import os, time
+import os
 
 app = Flask("__name__")
 
-
-
-app.config["IMAGE_UPLOADS"] = "static/img/"
-
-
-
 def engine_talk(text):
-    time.sleep(3)
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
 
-    
-    
     
 def user_commands():
     try:
@@ -53,31 +48,80 @@ def user_commands():
         pass
     return command
 
-
+def weather(city):
+    # Enter your API key here 
+    api_key = "<YOUR API KEY>"
+    #How to use api_key, see the below code:
+    #api_key = "ABCDE"
+    
+    # base_url variable to store url 
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    # Give city name 
+    city_name = city
+    
+    # complete_url variable to store 
+    # complete url address 
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
+    
+    # get method of requests module 
+    # return response object 
+    response = requests.get(complete_url) 
+    
+    # json method of response object  
+    # convert json format data into 
+    # python format data 
+    x = response.json() 
+    
+    # Now x contains list of nested dictionaries 
+    # Check the value of "cod" key is equal to 
+    # "404", means city is found otherwise, 
+    # city is not found 
+    if x["cod"] != "404": 
+    
+        # store the value of "main" 
+        # key in variable y 
+        y = x["main"] 
+    
+        # store the value corresponding 
+        # to the "temp" key of y 
+        current_temperature = y["temp"] 
+        temp_in_celcius = current_temperature - 273.15
+        return str(int(temp_in_celcius))
+    
+    
 def run_alexa():
     command = user_commands()
-    if 'play' in command:
+    if 'play a song' in command:
+        song = 'Arijit Singh'
+        engine_talk('Playing some music')
+        print("Playing....")
+        pywhatkit.playonyt(song)
+    elif 'play' in command:
         song = command.replace('play', '')
         engine_talk('Playing....' + song)
         print("Playing....")
-        pywhatkit.playonyt(song)
+        pywhatkit.playonyt(song)     
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         print(time)
-        
-        #engine_talk('Current Time is' + time)
-        pyttsx3.speak('Current Time is' + time)
+        engine_talk('Current Time is' + time)
     elif 'joke' in command:
         get_j = pyjokes.get_joke()
         print(get_j)
         engine_talk(get_j)
     elif 'who is' in command:
-        person = command.replace('who the heck is', '')
+        person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         engine_talk(info)
+    elif 'weather' in command:
+        #engine_talk('Please tell name of the city')
+        city = 'Hong Kong'
+        #city = 'Mumbai'
+        engine_talk('The temperature in Hong Kong is' + weather(city) + 'degree celcius')
     elif 'stop' in command:
-        sys.exit()
+        engine_talk("Good bye")
     else:
         engine_talk("I didn't hear you properly")
         print("I didn't hear you properly")
@@ -87,19 +131,16 @@ def run_alexa():
 def hello():
     return render_template("alexa.html")
 
-
-
-
 @app.route("/home")
 def home():
     return redirect('/')
 
-
-@app.route('/',methods=['POST'])
-def submit():   
-    run_alexa()
+@app.route('/',methods=['POST', 'GET'])
+def submit():
+    while True:
+        run_alexa()
     return render_template("alexa.html")
-    
-    
+        
+
 if __name__ =="__main__":
-    app.run()
+    app.run(debug=True)
